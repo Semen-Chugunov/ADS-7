d93b8fb2bd6f23c22d451231a9b40ce3e3039832
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-04-25 13:39:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,75 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    T* head = nullptr;
+
+ public:
+    void push(const T& data) {
+        if (isEmpty()) {
+            head = new T;
+		head->ch = data.ch;
+		head->prior = data.prior;
+            head->next = nullptr;
+        } else {
+          if (!head->next) {
+                T* temp = new T;
+                temp->ch = data.ch;
+                temp->prior = data.prior;
+                head->next = temp;
+                temp->next = nullptr;
+            } else { 
+              if (head->prior < data.prior) {
+                T* temp = new T;
+                temp->ch = data.ch;
+                temp->prior = data.prior;
+                temp->next = head;
+                head = temp;
+              } else {
+                T* cur = head;
+                while (cur->next && cur->next->prior >= data.prior) {
+                    cur = cur->next;
+                }
+                if (cur->next->prior < data.prior) {
+                        T* temp = new T;
+                        temp->ch = data.ch;
+                        temp->prior = data.prior;
+                        temp->next = cur->next;
+                        cur->next = temp;
+                } else {
+                  if (!cur->next) {
+                    T* temp = new T;
+                    temp->ch = data.ch;
+                    temp->prior = data.prior;
+                    cur->next = temp;
+                    temp->next = nullptr;
+                  }
+                }
+              }
+            }
+        }
+    }
+    const T& pop() {
+        if (isEmpty()) {
+            throw std::string("empty");
+        } else {
+            T* temp = head;
+            head = head->next;
+            return temp;
+        }
+    }
+    bool isEmpty() const {
+        return !head;
+    }
+
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
+    struct SYM* next;
 };
-
-#endif  // INCLUDE_TPQUEUE_H_
+#endif 
